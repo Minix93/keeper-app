2df55dcb04c85b045c5fa4888894e8a2a81c7990
Minix93 <mxjason93@gmail.com>
2022-10-05 16:54:58 -0700

    1. add mongoose module to server
    2. connected to local mongoDB through mongoose successfully and created a "test" database

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
